3b2962af416fe135a64cc09c338b92420c8877d8
HardikVagadiya28 <hardikvagadiya2003@gmail.com>
2025-09-27 23:53:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@react'
+import react from '@vitejs/plugin-react' // Fixed import name
 
 export default defineConfig({
   plugins: [react()],
   preview: {
     host: '0.0.0.0',
-    port: process.env.PORT || 4173, 
+    port: process.env.PORT || 4173,
     allowedHosts: [
-      'blogapp-frontend-dz8q.onrender.com' 
+      'blogapp-frontend-dz8q.onrender.com'
     ]
   },
   server: {
