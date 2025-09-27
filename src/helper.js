107c6013cd89ca36097d480fdce23aa795250922
HardikVagadiya28 <hardikvagadiya2003@gmail.com>
2025-09-27 23:37:34 +0530

    Update helper.js

--- src/helper.js
+++ src/helper.js
@@ -1,4 +1,1 @@
-export const api_base_url =
-  process.env.NODE_ENV === "production"
-    ? "https://blogapp-backend-2y8z.onrender.com"
-    : "http://localhost:2000";
+export const api_base_url = import.meta.env.VITE_API_URL || "http://localhost:2000";
